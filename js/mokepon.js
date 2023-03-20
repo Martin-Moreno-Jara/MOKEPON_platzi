57f9dc8469688d7118faabad8450bc02ddadbc88
Martin Moreno Jara <martin.moreno.j04@gmail.com>
2023-03-20 15:01:23 -0500

    js document created

---
+++ js/mokepon.js
@@ -0,0 +1,14 @@
+//Get elements from html +++++++++++++++++++++++
+let btnSelectMascot = document.getElementById("btnSelectMascot");
+let btnReload = document.getElementById("btnReload");
+let btnFire = document.getElementById("btnFire");
+let btnWater = document.getElementById("btnWater");
+let btnEarth = document.getElementById("btnEarth");
+
+//assign events for buttons and whatnot ++++++++++++
+btnSelectMascot.addEventListener("click",selectMascot)
+
+//Functions for the events +++++++++++++++++++++++++
+function selectMascot(){
+    console.log("ur gae");
+}
